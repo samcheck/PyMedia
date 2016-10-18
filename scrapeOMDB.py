7ef2891bcbff279e5d82ae8fe852b95f604e5ac8
samcheck <samcheckster@gmail.com>
2016-10-17 20:26:42 -0500

    Converting to a function

---
+++ scrapeOMDB.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# scrapeOMDB.py - parses a movie and year from arguments and returns JSON
+
+import json, requests
+
+URL_BASE = 'http://www.omdbapi.com/?'
+
+def OMDBmovie(mTitle, mYear):
+    # Craft the URL
+    url = URL_BASE + 't=' + mTitle + '&y=' + mYear + '&plot=full&r=json'
+
+    # Try to get the url
+    response = requests.get(url)
+    response.raise_for_status()
+
+    theJSON = json.loads(response.text)
+    return(theJSON)
