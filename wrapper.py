676494ab867101a9598a99773f04b61fb15effc0
samcheck <samcheckster@gmail.com>
2016-10-19 20:08:06 -0500

    Added checking for response returned from OMDB

--- wrapper.py
+++ wrapper.py
@@ -2,27 +2,40 @@
 # wrapper.py - get video path from command line inputs, combine together other
 # functions to build media db, in individual csv files
 
-import sys, csv, datetime
-import regSplit, scrapeOMDB, videoLister
+import sys
+import csv
+import tqdm
+from distutils.util import strtobool
+import regSplit
+import scrapeOMDB
+import videoLister
 
 in_path = ' '.join(sys.argv[1:])
-count = 0
+
 media_list = videoLister.videoDir(in_path)
-for item in media_list:
+files_no_response = []
+for item in tqdm.tqdm(media_list):
     reg_dict = regSplit.Split(item)
 
     if reg_dict['type'] == 'tv':
-        med_info = scrapeOMDB.OMDBtv(reg_dict['title'], reg_dict['season'], reg_dict['episode'])
+        med_info = scrapeOMDB.OMDBtv(reg_dict['title'], reg_dict['season'],
+                                    reg_dict['episode'])
+        if strtobool(med_info['Response']):
+            out_file = (reg_dict['title'] + '_S' + med_info['Season'].zfill(2) +
+                        'E' + med_info['Episode'].zfill(2) + '.csv')
+        else:
+            files_no_response.append(item)
 
     if reg_dict['type'] == 'movie':
         med_info = scrapeOMDB.OMDBmovie(reg_dict['title'], reg_dict['year'])
+        if strtobool(med_info['Response']):
+            out_file = (reg_dict['title'] + '_' + med_info['Year'] + '.csv')
+        else:
+            files_no_response.append(item)
 
-    count += 1
-    print('%s items processed...' % str(count))
-# Sort the headers into alphabetical order
-    headers = sorted(med_info)
-    out_file = reg_dict['title'] + '_' + datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S') + '.csv'
     with open(out_file, 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer = csv.DictWriter(csvfile, fieldnames=sorted(med_info))
         writer.writeheader()
         writer.writerow(med_info)
+
+print('Files without a response:', files_no_response)
