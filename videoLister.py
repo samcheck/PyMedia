c28848c1fd1cd74aba9905c78ab26446b2d8fc8a
samcheck <samcheckster@gmail.com>
2016-10-24 17:10:39 -0500

    convert to a generator

--- videoLister.py
+++ videoLister.py
@@ -8,26 +8,23 @@ VIDEO_EXT = ('.webm', '.mkv', '.flv', '.avi', '.mov', '.qt', '.wmv', '.mp4',
              '.m4v', '.mpg', '.mpeg')
 
 
-def videoDir(pathToVideos):
+def videoDir(path_to_videos):
     """Searches directory and generates a list of matching filenames.
 
     Argument:
-        pathToVideos: path to search for files matching the defined video
+        path_to_videos: path to search for files matching the defined video
             extensions.
 
-    Returns:
-        videoList: a list of matching filenames, including path.
+    Yields:
+        A matching filename, including path.
 
     Raises:
         Exception: Not a valid directory, please input a directory to search.
     """
-    if not(os.path.exists(pathToVideos) and os.path.isdir(pathToVideos)):
+    if not(os.path.exists(path_to_videos) and os.path.isdir(path_to_videos)):
         raise Exception('Not a valid directory, please input a directory to search.')
 
-    videoList = []
-    for root, dirs, files in os.walk(pathToVideos):
+    for root, dirs, files in os.walk(path_to_videos):
         for filename in files:
             if filename.endswith(VIDEO_EXT):
-                videoList.append((os.path.join(root, filename)))
-
-    return(videoList)
+                yield os.path.join(root, filename)
