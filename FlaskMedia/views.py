a8003a1b231e53e290fe8a8d45e682423be6c9c6
samcheck <samcheckster@gmail.com>
2016-11-02 22:02:09 -0500

    adding fake media posts

--- FlaskMedia/views.py
+++ FlaskMedia/views.py
@@ -4,5 +4,16 @@ from FlaskMedia import app
 @app.route('/')
 @app.route('/index')
 def index():
+	medialist = [
+	{
+		'title': 'The day the earth stood still',
+		'plot': 'centripedial motion stops'
+	},
+	{
+		'title': 'The Office (US)',
+		'plot': 'Michael Scott messes things up'
+	}
+	]
 	return render_template('index.html',
-							title='Home')
+							title='Home',
+							medialist=medialist)
