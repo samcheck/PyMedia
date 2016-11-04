33d085ff22e6674debb1c901615b8c6cddeeb7c9
samcheck <samcheckster@gmail.com>
2016-11-04 05:58:05 -0500

    changing .db name

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'FlaskMedia.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False # explictly set to suppress warnings
