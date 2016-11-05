907b75b276b97a7a75821a9583305a0257fdf6c3
samcheck <samcheckster@gmail.com>
2016-11-05 15:07:57 -0500

    adding randomized secret key

--- FlaskMedia/__init__.py
+++ FlaskMedia/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.secret_key = os.urandom(24)
+
 db = SQLAlchemy(app)
 
 from FlaskMedia import views, models
