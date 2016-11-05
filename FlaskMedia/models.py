0c97d621355f016a19f1d59ac41d3afb850b14e2
samcheck <samcheckster@gmail.com>
2016-11-05 15:08:58 -0500

    Separate Series and Episode structs

--- FlaskMedia/models.py
+++ FlaskMedia/models.py
@@ -1,12 +1,22 @@
 from FlaskMedia import db
 
-class TVshow(db.Model):
+class Series(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), index=True)
+    plot = db.Column(db.String(2048), index=True)
+    episodes = db.relationship('Episode', backref='series', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Series %r>' % (self.title)
+
+
+class Episode(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     season = db.Column(db.Integer, index=True)
     episode = db.Column(db.Integer, index=True)
-    series_title = db.Column(db.String(256), index=True)
-    ep_title = db.Column(db.String(256), index=True)
+    title = db.Column(db.String(256), index=True)
     plot = db.Column(db.String(2048), index=True)
+    series_id = db.Column(db.Integer, db.ForeignKey('series.id'))
 
     def __repr__(self):
-        return '<Episode %r>' % (self.ep_title)
+        return '<Episode %r>' % (self.title)
