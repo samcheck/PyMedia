0434a92d263820ca01dbfe0bfd732f48a0308089
samcheck <samcheckster@gmail.com>
2016-11-08 17:17:39 -0600

    moving SECRET_KEY into config

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+SECRET_KEY = os.urandom(24)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'FlaskMedia.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False # explictly set to suppress warnings
