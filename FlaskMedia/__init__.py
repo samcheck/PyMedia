aea2a973ee9a6d8e187b8cae0e3dc7014c4c4266
samcheck <samcheckster@gmail.com>
2016-11-08 17:19:44 -0600

    moving SECRECT_KEY into config

--- FlaskMedia/__init__.py
+++ FlaskMedia/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 app = Flask(__name__)
 app.config.from_object('config')
-app.secret_key = os.urandom(24)
 
 db = SQLAlchemy(app)
 
