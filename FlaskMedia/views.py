4efcee93c27804963137a1f03d2aa2c3862c5fdc
samcheck <samcheckster@gmail.com>
2016-11-08 17:22:00 -0600

    convert to using db querys and add a shutdown route

--- FlaskMedia/views.py
+++ FlaskMedia/views.py
@@ -4,30 +4,28 @@ from .models import Series, Episode
 
 @app.route('/')
 @app.route('/index')
+@app.route('/home')
 def index():
-	medialist = [
-	{
-		'series': {'title': 'The day the earth stood still'},
-		'episode': 'centripedial motion stops'
-	},
-	{
-		'series': {'title': 'The Office (US)'},
-		'episode': 'Michael Scott messes things up'
-	}
-	]
-	return render_template('index.html',
-							title='Home',
-                            episodes=medialist)
+    available_series = Series.query.all()
+    return render_template('index.html', title='Home', Series=available_series)
 
 
 @app.route('/TV/<series_title>')
+@app.route('/tv/<series_title>')
 def TV(series_title):
     TV = Series.query.filter_by(title=series_title).first()
     if not TV:
         flash('TV series: %s not found in database.' % series_title)
         return redirect(url_for('index'))
-    eps = [
-        {'series': TV, 'title': 'Episode one a new hope'},
-        {'series': TV, 'title': 'Episode two the empire strikes back'}
-        ]
+    eps = Episode.query.filter_by(series_id=TV.id).all()
     return render_template('TVshow.html', Series=TV, episodes=eps)
+
+
+
+@app.route('/shutdown', methods=['GET', 'POST'])
+def shutdown():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+    return 'Server shutting down...'
