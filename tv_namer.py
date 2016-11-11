3fba46db784aebced5d2dce6c42cd4899f663bb1
samcheck <samcheckster@gmail.com>
2016-11-11 17:59:39 -0600

    adding logging, removing some dependencies

--- tv_namer.py
+++ tv_namer.py
@@ -4,16 +4,21 @@
 import shutil
 import os
 import sys
-import tqdm
-from distutils.util import strtobool
+import logging
+
 import videoLister
 import scrapeTVDB
 import regSplit
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='tv_namer.log',level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
 in_path = ' '.join(sys.argv[1:])
 
-files_not_renamed = []
-for item in tqdm.tqdm(videoLister.videoDir(in_path)):
+for item in videoLister.videoDir(in_path):
+    logger.info("Working on: ", item)
     reg_dict = regSplit.Split(item)
     ext = os.path.splitext(item)[1]
 
@@ -24,10 +29,9 @@ for item in tqdm.tqdm(videoLister.videoDir(in_path)):
         new_name = (reg_dict['title'] + ' S' + reg_dict['season'].zfill(2) +
                         'E' + reg_dict['episode'].zfill(2) + ' - ' +
                         med_info['data'][0]['episodeName'] + ext)
+
+        logger.info("Renaming: %s, as: %s" % (item, new_name))
         shutil.move(item, (os.path.join(os.path.dirname(item), new_name)))
 
     else:
-        files_not_renamed.append(item)
-
-if files_not_renamed:
-    print('Files not renamed:', files_not_renamed)
+        logger.info("File not renamed: ", item)
