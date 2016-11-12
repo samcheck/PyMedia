52433ed72c5ff0913614d1ad076f8e2ca80842ff
samcheck <samcheckster@gmail.com>
2016-11-11 20:16:06 -0600

    adding first_aired, last_updated, and TVDB_id

--- write_to_db.py
+++ write_to_db.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import logging
+import datetime
 
 import videoLister
 import scrapeTVDB
@@ -14,9 +15,11 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(filename='write_to_db.log',level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-def write_ep(season, ep, title, plot, series_id):
+def write_ep(season, ep, title, plot, first_aired, last_updated_utc, TVDB_id, series_id):
     # make
-    new_ep=Episode(season=season, episode=ep, title=title, plot=plot, series_id=series_id)
+    new_ep=Episode(season=season, episode=ep, title=title, plot=plot,
+                   first_aired=first_aired, last_updated_utc=last_updated_utc,
+                   TVDB_id=TVDB_id, series_id=series_id)
     try:
         db.session.add(new_ep)
         db.session.commit()
@@ -53,23 +56,23 @@ for item in videoLister.videoDir(in_path):
         episode = med_info['data'][0]['airedEpisodeNumber']
         title = med_info['data'][0]['episodeName']
         plot = med_info['data'][0]['overview']
-
+        first_aired = datetime.datetime.strptime(med_info['data'][0]['firstAired'],'%Y-%m-%d')
+        last_updated_utc = datetime.datetime.utcnow()
+        TVDB_id = med_info['data'][0]['id']
         # unused data fields from theTVDB
         # 'absoluteNumber': (int)
         # 'airedSeasonID': (int) internal TVDB reference to season
-        # 'firstAired': 'YYYY-MM-DD'
-        # 'id': (int) internal TVDB reference
         # 'dvdSeason': 1,
         # 'dvdEpisodeNumber': 1,
 
         # search for series title in db
         series = Series.query.filter_by(title=series_title).first()
         if series: # make sure we get a match
-            write_ep(season, episode, title, plot, series.id)
+            write_ep(season, episode, title, plot, first_aired, last_updated_utc, TVDB_id, series.id)
         else:
             write_series(series_title)
             series = Series.query.filter_by(title=series_title).first()
-            write_ep(season, episode, title, plot, series.id)
+            write_ep(season, episode, title, plot, first_aired, last_updated_utc, TVDB_id, series.id)
 
     else:
         logger.warning('File not added: ', item)
