4d066384a95332103daefafc9f0ab15bd7bba3a2
samcheck <samcheckster@gmail.com>
2016-11-12 14:38:22 -0600

    adding write_ep_long for full metadata, and more precise episode query

--- write_to_db.py
+++ write_to_db.py
@@ -17,11 +17,29 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(filename='write_to_db.log',level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-def write_ep(season, ep, title, plot, first_aired, last_updated_utc, TVDB_id, series_id):
+def write_ep_short(season, ep, title, plot, first_aired, last_updated_utc, TVDB_id, series_id):
     # make
     new_ep=Episode(season=season, episode=ep, title=title, plot=plot,
                    first_aired=first_aired, last_updated_utc=last_updated_utc,
                    TVDB_id=TVDB_id, series_id=series_id)
+    # attempted to use try/except to catch errors but did not work on SQL errors
+    if not Episode.query.filter_by(season=season, episode=episode, series_id=series_id).first():
+        db.session.add(new_ep)
+        logger.info("Wrote Episode: %s" % new_ep)
+        db.session.commit()
+    else:
+        logger.warning("Episode %s already in database." % new_ep)
+
+
+def write_ep_long(season, ep, title, plot, first_aired, last_updated_utc,
+                  TVDB_id, IMDB_id, abs_num, TVDB_rating, TVDB_rating_count,
+                  TVDB_last_update, series_id):
+    # make
+    new_ep=Episode(season=season, episode=ep, title=title, plot=plot,
+                   first_aired=first_aired, last_updated_utc=last_updated_utc,
+                   TVDB_id=TVDB_id,IMDB_id=IMDB_id, abs_num=abs_num,
+                   TVDB_rating=TVDB_rating, TVDB_rating_count=TVDB_rating_count,
+                   TVDB_last_update=TVDB_last_update, series_id=series_id)
     # attempted to use try/except to catch errors but did not work on SQL errors
     if not Episode.query.filter_by(season=season, episode=episode, series_id=series_id).first():
         db.session.add(new_ep)
@@ -51,31 +69,39 @@ for item in tqdm(videoLister.videoDir(in_path)):
     logger.info("Working on: %s" % path)
 
     if reg_dict['type'] == 'tv':
-        med_info = scrapeTVDB.theTVDB(reg_dict['title'], reg_dict['season'],
+        ep_search = scrapeTVDB.theTVDB(reg_dict['title'], reg_dict['season'],
                                 reg_dict['episode'], JWT)
+
+
+        med_info = scrapeTVDB.episode_id(ep_search['data'][0]['id'], JWT)
         series_title = reg_dict['title']
         # get data using tvdb scraper
-        season = med_info['data'][0]['airedSeason']
-        episode = med_info['data'][0]['airedEpisodeNumber']
-        title = med_info['data'][0]['episodeName']
-        plot = med_info['data'][0]['overview']
-        first_aired = datetime.datetime.strptime(med_info['data'][0]['firstAired'],'%Y-%m-%d')
+        season = med_info['data']['airedSeason']
+        episode = med_info['data']['airedEpisodeNumber']
+        title = med_info['data']['episodeName']
+        plot = med_info['data']['overview']
+        first_aired = datetime.datetime.strptime(med_info['data']['firstAired'],'%Y-%m-%d')
         last_updated_utc = datetime.datetime.utcnow()
-        TVDB_id = med_info['data'][0]['id']
-        # unused data fields from theTVDB
-        # 'absoluteNumber': (int)
-        # 'airedSeasonID': (int) internal TVDB reference to season
-        # 'dvdSeason': 1,
-        # 'dvdEpisodeNumber': 1,
+        TVDB_id = med_info['data']['id']
+        IMDB_id = med_info['data']['imdbId']
+        abs_num = med_info['data']['absoluteNumber']
+        TVDB_rating = med_info['data']['siteRating']
+        TVDB_rating_count = med_info['data']['siteRatingCount']
+        TVDB_last_update = datetime.datetime.fromtimestamp(med_info['data']['lastUpdated'])
 
         # search for series title in db
         series = Series.query.filter_by(title=series_title).first()
         if series: # make sure we get a match
-            write_ep(season, episode, title, plot, first_aired, last_updated_utc, TVDB_id, series.id)
+            write_ep_long(season, episode, title, plot, first_aired,
+                          last_updated_utc, TVDB_id, IMDB_id, abs_num,
+                          TVDB_rating, TVDB_rating_count, TVDB_last_update,
+                          series.id)
         else:
             write_series(series_title)
             series = Series.query.filter_by(title=series_title).first()
-            write_ep(season, episode, title, plot, first_aired, last_updated_utc, TVDB_id, series.id)
-
+            write_ep_long(season, episode, title, plot, first_aired,
+                          last_updated_utc, TVDB_id, IMDB_id, abs_num,
+                          TVDB_rating, TVDB_rating_count, TVDB_last_update,
+                          series.id)
     else:
         logger.warning('File not added: ', item)
