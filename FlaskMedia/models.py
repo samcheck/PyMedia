fc5104ead5921f31354507efd3b62bf5a6221624
samcheck <samcheckster@gmail.com>
2016-11-12 14:35:36 -0600

    adding IMDB_id, absolute number, TVDB update time and TVDB rating/count

--- FlaskMedia/models.py
+++ FlaskMedia/models.py
@@ -19,6 +19,11 @@ class Episode(db.Model):
     first_aired = db.Column(db.DateTime)
     last_updated_utc = db.Column(db.DateTime)
     TVDB_id = db.Column(db.Integer, index=True)
+    IMDB_id = db.Column(db.String(15), index=True)
+    abs_num = db.Column(db.Integer, index=True)
+    TVDB_rating = db.Column(db.Float, index=True)
+    TVDB_rating_count = db.Column(db.Integer, index=True)
+    TVDB_last_update = db.Column(db.DateTime)
     series_id = db.Column(db.Integer, db.ForeignKey('series.id'))
     __table_args__ = (db.UniqueConstraint('season', 'episode', 'series_id', name='_episode_uc'),)
 
