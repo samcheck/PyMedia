9fcfdafb1ce01417bb07a9ed5557a5b44e706bcb
samcheck <samcheckster@gmail.com>
2016-11-14 17:48:55 -0600

    Convert to reuse JWT

--- tv_namer.py
+++ tv_namer.py
@@ -18,13 +18,15 @@ logging.basicConfig(filename='tv_namer.log',level=logging.DEBUG,
 in_path = ' '.join(sys.argv[1:])
 
 for item in videoLister.videoDir(in_path):
-    logger.info("Working on: ", item)
+    logger.info(("Working on: " + os.path.abspath(item)))
     reg_dict = regSplit.Split(item)
     ext = os.path.splitext(item)[1]
 
+    JWT = scrapeTVDB.auth()
+
     if reg_dict['type'] == 'tv':
         med_info = scrapeTVDB.theTVDB(reg_dict['title'], reg_dict['season'],
-                                    reg_dict['episode'])
+                                    reg_dict['episode'], JWT)
 
         new_name = (reg_dict['title'] + ' S' + reg_dict['season'].zfill(2) +
                         'E' + reg_dict['episode'].zfill(2) + ' - ' +
@@ -34,4 +36,4 @@ for item in videoLister.videoDir(in_path):
         shutil.move(item, (os.path.join(os.path.dirname(item), new_name)))
 
     else:
-        logger.warning("File not renamed: ", item)
+        logger.warning(("File not renamed: "+ os.path.abspath(item)))
