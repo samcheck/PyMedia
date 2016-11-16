25686cd80717fd9bf2e7320902a163c432c18a74
samcheck <samcheckster@gmail.com>
2016-11-15 18:27:04 -0600

    Adding logging and switch shutil.move to os.rename

--- media_namer.py
+++ media_namer.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
 # media_namer.py - Renames passed media files
 
-import shutil
+
 import os
 import sys
-import tqdm
+import logging
 from distutils.util import strtobool
+
+import tqdm
+
 import videoLister
 import scrapeOMDB
 import regSplit
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='media_namer.log',level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 in_path = ' '.join(sys.argv[1:])
 
-files_no_response = []
 for item in tqdm.tqdm(videoLister.videoDir(in_path)):
-    med_info = {}
     reg_dict = regSplit.Split(item)
     ext = os.path.splitext(item)[1]
 
@@ -26,18 +31,17 @@ for item in tqdm.tqdm(videoLister.videoDir(in_path)):
             new_name = (reg_dict['title'] + ' S' + med_info['Season'].zfill(2) +
                         'E' + med_info['Episode'].zfill(2) + ' - ' +
                         med_info['Title'] + ext)
-            shutil.move(item, (os.path.join(os.path.dirname(item), new_name)))
+            logger.info("Renaming: %s, as: %s" % (item, new_name))
+            os.rename(item, (os.path.join(os.path.dirname(item), new_name)))
         else:
-            files_no_response.append(item)
+            logger.warning(("File not renamed: "+ os.path.abspath(item)))
 
     if reg_dict['type'] == 'movie':
         med_info = scrapeOMDB.OMDBmovie(reg_dict['title'], reg_dict['year'])
 
         if strtobool(med_info['Response']):
             new_name = (reg_dict['title'] + '(' + med_info['Year'] + ')' + ext)
-            shutil.move(item, (os.path.join(os.path.dirname(item), new_name)))
+            logger.info("Renaming: %s, as: %s" % (item, new_name))
+            os.rename(item, (os.path.join(os.path.dirname(item), new_name)))
         else:
-            files_no_response.append(item)
-
-if files_no_response:
-    print('Files without a response:', files_no_response)
+            logger.warning(("File not renamed: "+ os.path.abspath(item)))
