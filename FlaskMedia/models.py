2fe94b3f56d8f0a9f1ed0d4c9c1f2b9094d6d13c
samcheck <samcheckster@gmail.com>
2016-11-15 20:32:53 -0600

    adding Movie db.Model class

--- FlaskMedia/models.py
+++ FlaskMedia/models.py
@@ -1,6 +1,7 @@
 from FlaskMedia import db
 
 class Series(db.Model):
+    """DB structure for TV Series."""
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(256), index=True)
     plot = db.Column(db.String(2048), index=True)
@@ -11,6 +12,7 @@ class Series(db.Model):
 
 
 class Episode(db.Model):
+    """DB structure for TV Episodes."""
     id = db.Column(db.Integer, primary_key=True)
     season = db.Column(db.Integer, index=True)
     episode = db.Column(db.Integer, index=True)
@@ -29,3 +31,25 @@ class Episode(db.Model):
 
     def __repr__(self):
         return '<S%sE%s - %r>' % (self.season, self.episode, self.title)
+
+
+class Movie(db.Model):
+    """DB structure for Movies."""
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), index=True)
+    plot = db.Column(db.String(2047), index=True)
+    year = db.Column(db.Integer, index=True)
+    released = db.Column(db.DateTime)
+    last_updated_utc = db.Column(db.DateTime)
+    rated = db.Column(db.String(15), index=True)
+    runtime = db.Column(db.String(15), index=True)
+    IMDB_id = db.Column(db.String(15), index=True)
+    IMDB_rating = db.Column(db.Float, index=True)
+    IMDB_rating_count = db.Column(db.Integer, index=True)
+    metascore = db.Column(db.Integer, index=True)
+    awards = db.Column(db.String(255), index=True)
+    language = db.Column(db.String(15), index=True)
+    country = db.Column(db.String(31), index=True)
+
+    def __repr__(self):
+        return '<%s (%d)>' % (self.title, self.year)   
