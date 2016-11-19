b006c4c759d609520257a1743dc8fcb3d4a5f58f
samcheck <samcheckster@gmail.com>
2016-11-18 18:00:07 -0600

    adding Movie model to be erased

--- db_erase.py
+++ db_erase.py
@@ -1,12 +1,14 @@
 #!venv/bin/python3
 from FlaskMedia import db
-from FlaskMedia.models import Series
-from FlaskMedia.models import Episode
+from FlaskMedia.models import Series, Episode, Movie
 
 for ep in Episode.query.all():
     db.session.delete(ep)
 
 for series in Series.query.all():
     db.session.delete(series)
+
+for mov in Movie.query.all():
+    db.session.delete(mov)
 
 db.session.commit()
