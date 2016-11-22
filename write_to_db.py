25e54b797b0c91e84555c97ba4bb15b581d333e1
samcheck <samcheckster@gmail.com>
2016-11-22 13:26:59 -0600

    combine tv/movie write to db in single script

--- write_to_db.py
+++ write_to_db.py
@@ -3,67 +3,22 @@ import os
 import sys
 import logging
 import datetime
+from distutils.util import strtobool
 
 from tqdm import tqdm
 
 import videoLister
 import scrapeTVDB
+import scrapeOMDB
 import regSplit
+from media_write import write_ep_long, write_series, write_movie
 
 from FlaskMedia import db
-from FlaskMedia.models import Series, Episode
+from FlaskMedia.models import Series
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='write_to_db.log',level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-def write_ep_short(season, ep, title, plot, first_aired, last_updated_utc, TVDB_id, series_id):
-    # make
-    new_ep=Episode(season=season, episode=ep, title=title, plot=plot,
-                   first_aired=first_aired, last_updated_utc=last_updated_utc,
-                   TVDB_id=TVDB_id, series_id=series_id)
-    # attempted to use try/except to catch errors but did not work on SQL errors
-    if not Episode.query.filter_by(season=season, episode=episode, series_id=series_id).first():
-        db.session.add(new_ep)
-        logger.info("Wrote Episode: %s" % new_ep)
-        db.session.commit()
-    else:
-        logger.warning("Episode %s already in database." % new_ep)
-
-
-def write_ep_long(season, ep, title, plot, first_aired, last_updated_utc,
-                  TVDB_id, IMDB_id, abs_num, TVDB_rating, TVDB_rating_count,
-                  TVDB_last_update, series_id):
-    # make
-    new_ep=Episode(season=season, episode=ep, title=title, plot=plot,
-                   first_aired=first_aired, last_updated_utc=last_updated_utc,
-                   TVDB_id=TVDB_id,IMDB_id=IMDB_id, abs_num=abs_num,
-                   TVDB_rating=TVDB_rating, TVDB_rating_count=TVDB_rating_count,
-                   TVDB_last_update=TVDB_last_update, series_id=series_id)
-    # attempted to use try/except to catch errors but did not work on SQL errors
-    if not Episode.query.filter_by(season=season, episode=episode, series_id=series_id).first():
-        db.session.add(new_ep)
-        logger.info("Wrote Episode: %s" % new_ep)
-        db.session.commit()
-    else:
-        logger.warning("Episode %s already in database." % new_ep)
-
-
-def write_series(title, plot, rated, IMDB_id, TVDB_rating, TVDB_rating_count,
-                 TVDB_last_update, first_aired, last_updated_utc, poster,
-                 network):
-
-    new_series=Series(title=title, plot=plot, rated=rated, IMDB_id=IMDB_id,
-                      TVDB_rating=TVDB_rating, TVDB_rating_count=TVDB_rating_count,
-                      TVDB_last_update=TVDB_last_update, first_aired=first_aired,
-                      last_updated_utc=last_updated_utc, poster=poster, network=network)
-    # attempted to use try/except to catch errors but did not work on SQL errors
-    if not Series.query.filter_by(title=title).first():
-        db.session.add(new_series)
-        logger.info("Wrote Series: %s" % title)
-        db.session.commit()
-    else:
-        logger.warning("Series %s already in database." % new_series)
 
 
 in_path = ' '.join(sys.argv[1:])
@@ -129,5 +84,39 @@ for item in tqdm(videoLister.videoDir(in_path)):
                           last_updated_utc, TVDB_id, IMDB_id, abs_num,
                           TVDB_rating, TVDB_rating_count, TVDB_last_update,
                           series.id)
+
+    if reg_dict['type'] == 'movie':
+        med_info = scrapeOMDB.OMDBmovie(reg_dict['title'], reg_dict['year'])
+        if strtobool(med_info['Response']):
+            # get data using OMDB scraper
+            title = med_info['Title']
+            plot = med_info['Plot']
+            year = int(med_info['Year'])
+            released = datetime.datetime.strptime(med_info['Released'],'%d %b %Y')
+            last_updated_utc = datetime.datetime.utcnow()
+            rated = med_info['Rated']
+            runtime = med_info['Runtime']
+            IMDB_id = med_info['imdbID']
+            if med_info['imdbRating'].isdigit():
+                IMDB_rating = med_info['imdbRating'] # need to check if N/A before convert to float
+            else:
+                IMDB_rating = 0
+            if med_info['imdbRating'].isdigit():
+                IMDB_rating_count = med_info['imdbVotes'] #need to parse string remove , before convert to int
+            else:
+                IMDB_rating_count = 0
+            if med_info['Metascore'].isdigit():
+                metascore = med_info['Metascore'] # need to check if N/A before convert to int
+            else:
+                metascore = 0
+            awards = med_info['Awards']
+            language = med_info['Language']
+            country = med_info['Country']
+            poster = med_info['Poster']
+
+            write_movie(title, plot, year, released, last_updated_utc, rated,
+                runtime, IMDB_id, IMDB_rating, IMDB_rating_count, metascore,
+                awards, language, country, poster)
+
     else:
-        logger.warning('File not added: ', item)
+        logger.warning('File not added: %s' % item)
