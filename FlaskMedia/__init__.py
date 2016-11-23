ac5c991a91f10de5dfb868793c5f929e186a3113
samcheck <samcheckster@gmail.com>
2016-11-23 10:24:35 -0600

    import momentjs/wrapper

--- FlaskMedia/__init__.py
+++ FlaskMedia/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from .momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+app.jinja_env.globals['momentjs'] = momentjs
 
 db = SQLAlchemy(app)
 
