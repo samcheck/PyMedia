8ffa97939ee5e6e597fae54e48ce002dfde1a07a
samcheck <samcheckster@gmail.com>
2017-03-06 21:17:54 -0600

    adding option to select by time

--- whats_on_tv.py
+++ whats_on_tv.py
@@ -11,17 +11,19 @@ import subprocess
 import shlex
 
 import videoLister
+import mff
 
 def main():
     # set up logging
     logger = logging.getLogger(__name__)
-    logging.basicConfig(filename='whats_on_tv.log', filemode='w', level=logging.WARNING,
+    logging.basicConfig(filename='whats_on_tv.log', filemode='w', level=logging.INFO,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
     # set up argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', help='Input directory to search.')
+    parser.add_argument('-t', '--time', help='Viewing time in minutes')
     args = parser.parse_args()
     if args.input:
         in_path = args.input
@@ -30,13 +32,25 @@ def main():
         sys.exit(1)
 
     # One time loop to generate a list of available media files in path
-    mList = []
+    m_list = []
     for item in videoLister.videoDir(in_path):
-        logger.info("Found: {}".format(item))
-        mList.append(item)
+        #logger.info("Found: {}".format(item)) # Can write really long log files
+        m_list.append(item)
 
     # Randomly select a video to play
-    choice = random.choice(mList)
+
+    if args.time: # Find a file with a duration shorter than allotted time
+        duration = 999
+        while duration > int(args.time):
+            choice = random.choice(m_list)
+            m_list.remove(choice) # remove the choice from the list
+            m_file = mff.format_info(choice) # get file details
+            duration = round(float(m_file['duration']) / 60) # convert to integer minutes
+            logger.info("Selected: {}".format(os.path.basename(choice)))
+            logger.info("Running time: {} min".format(duration))
+    else:
+        choice = random.choice(m_list)
+
     logger.info("Playing: {}".format(os.path.basename(choice)))
 
     # Launch selected video with MPV in full screen
